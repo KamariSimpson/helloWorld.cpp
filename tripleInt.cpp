29c24949ccdc3f7d2535faadd9b715817639b088
KamariSimpson <bktmari1@gmail.com>
2025-01-13 08:34:56 -0800

    tripleInt.cpp

---
+++ tripleInt.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    int num;
+    std::cout << "Enter an integer: ";
+    std::cin >> num;
+    std::cout << "Triple of " << num << " is " << num * 3 << std::endl;
+    return 0;
+}
